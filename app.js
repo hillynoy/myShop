b3fe25f1f355be822561d4b06a44c8f762a1d7cb
hilly noy <hilly88@gmail.com>
2016-07-29 17:10:28 +0300

    fixed- issue #1 folder structure issue #4 directive implementation

--- app.js
+++ app.js
@@ -20,52 +20,6 @@
                 })
                 .otherwise({redirectTo: '/'})
         });
-
-    app.controller("StoreController", function ($scope, $location, $routeParams, $http) {
-
-        $http({
-            method: 'GET',
-            url: 'https://api.myjson.com/bins/20cqh'}).then(function successCallback(response) {
-            $scope.products = response.data;
-            if ($routeParams.id) {
-                $scope.p = $scope.products.filter(function (item) {
-                    return item.id == $routeParams.id;
-                })[0];
-            }
-
-
-
-        }, function errorCallback(response) {
-        });
-
-        if( $scope.cart ===undefined)
-        {
-            if (localStorage.getItem("cart")) {
-                $scope.cart = JSON.parse(localStorage.getItem("cart"));
-                console.log($scope.cart);
-            }
-        }
-        /*   localStorage.removeItem("cart");
-         $scope.cart = [];
-
-         if (localStorage.getItem("cart")) {
-         $scope.cart = JSON.parse(localStorage.getItem("cart"));
-         }*/
-
-
-
-        $scope.addItem = function (p) {
-            p.addedToCart = true;
-            $scope.cart.push(p);
-            //localStorage.removeItem("cart");
-            localStorage.setItem("cart", JSON.stringify($scope.cart));
-            console.log($scope.cart);
-        };
-
-        $scope.countItems = function (product) {
-            product.count++;
-        }
-    });
 })
 
 ();
