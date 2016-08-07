a78a1635a4c9a13994b79892730d2afe0f9e8469
hilly noy <hilly88@gmail.com>
2016-08-07 12:19:47 +0300

    shop - cart works again

--- directives/directives.js
+++ directives/directives.js
@@ -1,50 +1,62 @@
 (function () {
 
-var app = angular.module("myShop");
-app.controller("StoreController", function ($scope, $location, $routeParams, $http) {
+    var app = angular.module("myShop");
+    app.controller("StoreController", function ($scope, $location, $routeParams, $http) {
 
-    $http({
-        method: 'GET',
-        url: 'https://api.myjson.com/bins/20cqh'}).then(function successCallback(response) {
-        $scope.products = response.data;
-        if ($routeParams.id) {
-            $scope.p = $scope.products.filter(function (item) {
-                return item.id == $routeParams.id;
-            })[0];
+        angular.element(document).ready(function () {
+            init();
+        });
+
+        function init() {
+            if ($scope.cart === undefined || $scope.cart == []) {
+                if (localStorage.getItem("cart")) {
+                    $scope.cart = JSON.parse(localStorage.getItem("cart"));
+                    console.log($scope.cart);
+                }
+            }
+
+            if ($scope.products == undefined) {
+                $http({
+                    method: 'GET',
+                    url: 'https://api.myjson.com/bins/20cqh'
+                }).then(
+                    function successCallback(response) {
+
+                        $scope.products = response.data;
+                        if ($routeParams.id) {
+                            $scope.p = $scope.products.filter(function (item) {
+                                return item.id == $routeParams.id;
+                            })[0];
+                        }
+                    },
+                    function errorCallback(response) {
+                    }
+                );
+            }
         }
 
+       
+
+        /*   localStorage.removeItem("cart");
+         $scope.cart = [];
+
+         if (localStorage.getItem("cart")) {
+         $scope.cart = JSON.parse(localStorage.getItem("cart"));
+         }*/
 
 
-    }, function errorCallback(response) {
-    });
-
-    if( $scope.cart ===undefined)
-    {
-        if (localStorage.getItem("cart")) {
-            $scope.cart = JSON.parse(localStorage.getItem("cart"));
+        $scope.addItem = function (p) {
+            p.addedToCart = true;
+            $scope.cart.push(p);
+            //localStorage.removeItem("cart");
+            localStorage.setItem("cart", JSON.stringify($scope.cart));
             console.log($scope.cart);
+        };
+
+        $scope.countItems = function (product) {
+            product.count++;
         }
-    }
-    /*   localStorage.removeItem("cart");
-     $scope.cart = [];
-    
-     if (localStorage.getItem("cart")) {
-     $scope.cart = JSON.parse(localStorage.getItem("cart"));
-     }*/
-
-
-
-    $scope.addItem = function (p) {
-        p.addedToCart = true;
-        $scope.cart.push(p);
-        //localStorage.removeItem("cart");
-        localStorage.setItem("cart", JSON.stringify($scope.cart));
-        console.log($scope.cart);
-    };
-
-    $scope.countItems = function (product) {
-        product.count++;
-    }
-});
+    })
+    ;
 
 })();
